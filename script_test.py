3fe423bb5086faa84c57e4d03b1aa5cb05205a9e
Massimo Romano <romano.massimo.uni@gmail.com>
2026-02-27 15:21:22 +0100

    change

--- script_test.py
+++ script_test.py
@@ -1,18 +1,20 @@
-from transformers import AutoModelForVision2Seq, AutoProcessor
 import torch
+from transformers import AutoModelForVision2Seq, AutoProcessor
 
+model_id = "openvla/openvla-7b"
 
-# Caricamento in 4-bit usando bitsandbytes
 try:
-    processor = AutoProcessor.from_pretrained("openvla/openvla-7b", trust_remote_code=True)
+    processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
+    
     vla = AutoModelForVision2Seq.from_pretrained(
-    "openvla/openvla-7b",
-    #attn_implementation="flash_attention_2",  # [Optional] Requires `flash_attn`
-    torch_dtype=torch.bfloat16, 
-    low_cpu_mem_usage=True, 
-    device_map="auto",
-    load_in_4bit=True,
-    ).to("cuda:0")
-    print("Successo! Il modello è in memoria (4-bit).")
+        model_id,
+        torch_dtype=torch.float16,  # Usiamo FP16, supportato dalla Titan Xp
+        low_cpu_mem_usage=True, 
+        device_map="auto",          # Gestisce lui l'allocazione
+        trust_remote_code=True
+    )
+    print("Successo! Il modello è in memoria in modalità FP16.")
+    
 except Exception as e:
-    print(f"Errore OOM o hardware: {e}")
+    print(f"Errore: {e}")
+    # Se qui ricevi Out of Memory, significa che i 16GB sono troppi pochi
