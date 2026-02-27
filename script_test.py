8d7fbc329f7238ac9b797c6bc8795a0f85db3cd0
Massimo Romano <romano.massimo.uni@gmail.com>
2026-02-27 12:57:54 +0100

    update test script

--- script_test.py
+++ script_test.py
@@ -1,17 +1,18 @@
+from transformers import AutoModelForVision2Seq, AutoProcessor
 import torch
-from transformers import AutoConfig, AutoModelForVision2Seq
 
-model_id = "openvla/openvla-7b"
 
 # Caricamento in 4-bit usando bitsandbytes
 try:
-    model = AutoModelForVision2Seq.from_pretrained(
-        model_id,
-        device_map="auto",
-        load_in_4bit=True,
-        torch_dtype=torch.bfloat16,
-        low_cpu_mem_usage=True
-    )
+    processor = AutoProcessor.from_pretrained("openvla/openvla-7b", trust_remote_code=True)
+    vla = AutoModelForVision2Seq.from_pretrained(
+    "openvla/openvla-7b",
+    #attn_implementation="flash_attention_2",  # [Optional] Requires `flash_attn`
+    torch_dtype=torch.bfloat16, 
+    low_cpu_mem_usage=True, 
+    device_map="auto",
+    load_in_4bit=True,
+    ).to("cuda:0")
     print("Successo! Il modello è in memoria (4-bit).")
 except Exception as e:
     print(f"Errore OOM o hardware: {e}")
