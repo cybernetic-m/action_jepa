66f67bd3d54ca4a51c4bc05fb55de4ce77f53fc9
Massimo Romano <romano.2043836@studenti.uniroma1.it>
2026-03-02 17:03:49 +0100

    download from huggingface the models

--- download_models.py
+++ download_models.py
@@ -4,45 +4,7 @@
 # Data: 2026
 
 import os
-import requests
-from tqdm import tqdm
-from transformers import AutoVideoProcessor, AutoModel
 from huggingface_hub import snapshot_download
-
-# --- DOWNLOAD FUNCTION ---- #
-def download_file(url, output_path):
-# Description: Download a file from a given URL and save it to the specified output path.
-# Args:
-#   url (str): The URL of the file to be downloaded.
-#   output_path (str): The local file path where the downloaded file will be saved.
-    try:
-        # Send a GET request to the specified URL with streaming enabled (it allows downloading large files without loading them entirely into RAM memory)
-        response = requests.get(url, stream=True)
-
-        # Get the total size of the file from the response headers, this is used for tqdm bar
-        total_size = int(response.headers.get('content-length', 0)) 
-
-        # Open the output file in binary write mode and write the content of the response to it in chunks, while updating the tqdm progress bar
-        # The tqdm bar will divide the total size of the file in 1024, 1024*1024, etc. and will display the most scaled unit (KB, MB, etc.) in the progress bar
-        # Es. File size = 100 000 000 bytes, tqdm will display the progress bar in MB (100 000 000 / 1024 / 1024 = 95.37 MB) 
-        with open(output_path, 'wb') as file:
-            with tqdm(desc=f"Downloading {output_path}", total=total_size, unit='B', unit_scale=True, unit_divisor=1024) as bar:
-                for data in response.iter_content(chunk_size=1024):
-                    size = file.write(data) # write the chunk of data to the file, returning the size of the chunk just written
-                    bar.update(size) # update the tqdm bar with the size of the chunk just written
-        print(f"File downloaded successfully and saved to: {output_path}")
-
-    # Handling different types of exceptions that may occur during the download process and printing appropriate error messages
-    # For example 404 Not Found, connection issues, timeouts, or generic errors.
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred while downloading the file: {http_err}")
-    except requests.exceptions.ConnectionError as conn_err:
-        print(f"Connection error occurred while downloading the file: {conn_err}")
-    except requests.exceptions.Timeout as timeout_err:
-        print(f"Timeout error occurred while downloading the file: {timeout_err}")
-    except Exception as err:
-        print(f"A generic error occurred while downloading the file: {err}")
-
 
 # --- DOWNLOAD MODELS FUNCTION ---- #
 def hf_download_models(hf_repo_id_list):
@@ -55,21 +17,21 @@ def hf_download_models(hf_repo_id_list):
     target_dir = "checkpoints" # the directory where the downloaded models will be saved 
 
     for repo_id in hf_repo_id_list:
-        print(f"Downloading model from Hugging Face repository: {repo_id}")
+        print(f"\nDownloading model from Hugging Face repository: {repo_id}\n")
         
         local_dir = os.path.join(target_dir, repo_id) # the full local path where the file will be saved (Ex. checkpoints/vjepa2_ac_droid.pth.tar)
 
         # Create the target directory if it does not exist, otherwise print that the directory already exists
         if not os.path.exists(target_dir):
             os.makedirs(target_dir)
-            print(f"Created directory: {target_dir}")
+            print(f"\nCreated directory: {target_dir}\n")
         else: 
-            print(f"Directory already exists: {target_dir}")
-            continue
+            print(f"\nDirectory already exists: {target_dir}\n")
+        
         
         # Check if the file already exists at the local path. 
         if os.path.exists(local_dir):
-            print(f"File already exists at: {local_dir}")
+            print(f"\nFile already exists at: {local_dir}\n")
             continue
 
         # Call the download function of Hugging Face to download the file from the specified URL and save it to the local path
@@ -88,5 +50,6 @@ if __name__ == "__main__":
                     hf_repo_id_list=[
                         "facebook/vjepa2-vith-fpc64-256",
                         "google-bert/bert-base-uncased"
+                    ]
                     )
-    print(f"Pretrained model weights downloaded and saved at: {weights_path}")
+    print(f"\nPretrained model weights downloaded and saved at: {weights_path}\n")
--- model/modules/vjepa2.py
+++ model/modules/vjepa2.py
@@ -1,13 +1,8 @@
 from transformers import AutoVideoProcessor, AutoModel
 
-hf_repo = "facebook/vjepa2-vitg-fpc64-256"
-# facebook/vjepa2-vitl-fpc64-256
-# facebook/vjepa2-vith-fpc64-256
-# facebook/vjepa2-vitg-fpc64-256
-# facebook/vjepa2-vitg-fpc64-384
+model_path = "./checkpoints/facebook/vjepa2-vith-fpc64-256" # the local path where the downloaded model weights are saved, for example "checkpoints/facebook/vjepa2-vith-fpc64-256"
 
-
-model = AutoModel.from_pretrained(hf_repo) 
-processor = AutoVideoProcessor.from_pretrained(hf_repo)
+model = AutoModel.from_pretrained(model_path, local_files_only=True) # load the model weights from the local path, setting local_files_only to True to avoid trying to download the weights from Hugging Face if they are not found at the local path
+processor = AutoVideoProcessor.from_pretrained(model_path, local_files_only=True)
 
 print(model)
