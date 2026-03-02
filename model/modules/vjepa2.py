3360156458176467017b7c4270edfaabcb5a444c
Massimo Romano <romano.2043836@studenti.uniroma1.it>
2026-03-02 13:11:01 +0100

    to not push checkpoints

--- model/modules/vjepa2.py
+++ model/modules/vjepa2.py
@@ -7,7 +7,7 @@ hf_repo = "facebook/vjepa2-vitg-fpc64-256"
 # facebook/vjepa2-vitg-fpc64-384
 
 
-model = AutoModel.from_pretrained(hf_repo)
+model = AutoModel.from_pretrained(hf_repo) 
 processor = AutoVideoProcessor.from_pretrained(hf_repo)
 
 print(model)
