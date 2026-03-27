e169dc3af56efa1695e99e2778546a14eead2c50
Massimo Romano <romano.2043836@studenti.uniroma1.it>
2026-03-27 15:58:38 +0100

    libero dataset update

--- Dataset/LiberoDataset.py
+++ Dataset/LiberoDataset.py
@@ -2,7 +2,14 @@ import os
 from torch.utils.data import Dataset
 
 class LiberoDataset(Dataset):
-    def __init__(self, data_dir):
+    def __init__(self, data_dir, selected_tasks):
         super(LiberoDataset, self).__init__()
 
-        self.data_dir
+        self.file_paths = []    # a list where to append all the demo file .pt to do torch.load
+        self.data_dir_list
+
+    def __len__(self):
+        return len()
+    
+    def __getitem__(self, index):
+        return index
